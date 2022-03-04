b071182091832f2836e36a9306438fb5f61f5c9d
vvzzen <alvin.dhea@gmail.com>
2022-03-04 19:34:38 +0700

    kategori finish v1

--- src/Components/Common/Navbar.js
+++ src/Components/Common/Navbar.js
@@ -3,15 +3,15 @@ import Logo from "../../Assets/logo.png";
 const Navbar = () => {
   return (
     <div className="fixed top-0 right-0 left-0 z-50 w-full bg-gray-100 h-24">
-      <div className="mx-auto container flex mt-3">
+      <div className="mx-auto container flex mt-4">
         <div className="w-1/2">
           <img className=" pt-5 h-auto" alt="" src={Logo} />
         </div>
         <div className="pt-4 w-full flex justify-end ">
-          <button className="text-xl border py-1 px-4 rounded-xl text-blue-700 font-bold border-blue-700">
+          <button className="text-xl border pt-1 pb-1.5 px-4 rounded-xl text-blue-700 font-bold border-blue-700">
             Masuk
           </button>
-          <button className="text-xl ml-4 border py-1 px-4 rounded-xl text-white font-bold bg-blue-700">
+          <button className="text-xl ml-4 border pt-1 pb-1.5 px-4 rounded-xl text-white font-bold bg-blue-700">
             Daftar
           </button>
         </div>
--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -2,6 +2,14 @@ import React from "react";
 import Navbar from "../Components/Common/Navbar";
 import Footer from "../Components/Common/Footer";
 import Hero1 from '../Assets/hero1.png'
+import Kategori from '../Assets/kategori.png'
+import Mobil from '../Assets/mobil.png'
+import Konsol from '../Assets/game.png'
+import Musik from '../Assets/gitar.png'
+import Motor from '../Assets/motor.png'
+import Foto from '../Assets/kamera.png'
+import Buku from '../Assets/buku.png'
+import LihatS from '../Assets/Button_Lihat Semua.png'
 import { useState } from "react";
 
 function Homepage() {
@@ -12,14 +20,47 @@ function Homepage() {
       <Navbar />
       <div className="mx-auto container flex justify-center mt-36 h-full w-full">
         <img src={Hero1} alt="" className="z-10 absolute"/>
-        <div class="z-40 relative container mx-auto flex flex-col items-center justify-center mt-52 p-12 text-gray-900 antialiased">
-          <div class="mb-8 px-6 py-3 border border-gray-200 bg-white max-w-lg max-h-min border-opacity-75 rounded-lg shadow-lg w-full space-x-6 flex items-center">
+        <div class="z-40 relative container mx-auto flex flex-col items-center justify-center my-52 p-12 text-gray-900 antialiased">
+          <div class="mb-8 px-6 py-3 border border-gray-200 bg-white max-w-lg max-h-min border-opacity-75 rounded-lg shadow-lg w-full space-x-6 flex items-center transform transition duration-300 md:hover:scale-105">
             <input type="search" class="w-full bg-transparent text-base focus:outline-none" placeholder="Apa yang anda cari?"/>
-            <button class="focus:outline-none"><i class="fa fa-search text-gray-500"></i></button>
+            <button class="focus:outline-none transform transition duration-300 md:hover:scale-105"><i class="fa fa-search text-gray-500"></i></button>
           </div>
         </div>
       </div>
-      <div></div>
+      <div className="bg-gray-100 py-12 mt-8">
+        <div className="mx-auto container">
+          <img src={Kategori} alt="" />
+          <div className="pt-6 flex justify-center space-x-3">
+            <button className="transform transition duration-300 md:hover:scale-105">
+              <img src={Mobil} alt="" />
+            </button>
+            <div className="flex-col space-y-4 w-52"> 
+              <button className="transform transition duration-300 md:hover:scale-105">
+                <img src={Musik} alt="" />
+              </button>
+              <button className="transform transition duration-300 md:hover:scale-105"> 
+                <img src={Foto} alt="" />
+              </button> 
+            </div>
+            <div className="flex-col space-y-4 w-52"> 
+              <button className="transform transition duration-300 md:hover:scale-105">
+                <img src={Konsol} alt="" /> 
+              </button>
+              <button className="transform transition duration-300 md:hover:scale-105">
+                <img src={Buku} alt="" /> 
+              </button>
+            </div> 
+            <div className="flex-col space-y-4 w-52"> 
+              <button className="transform transition duration-300 md:hover:scale-105">
+                <img src={Motor} alt="" /> 
+              </button>
+              <button className="transform transition duration-300 md:hover:scale-105">
+                <img src={LihatS} alt="" />
+              </button> 
+            </div>
+          </div>
+        </div>
+      </div>
       <div></div>
       {/* <Footer /> */}
     </div>
