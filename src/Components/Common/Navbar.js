71b2593f0a14d46b2f0e6de76d8273634c9ad8f1
vvzzen <alvin.dhea@gmail.com>
2022-03-04 17:59:38 +0700

    visual hero finish 1

--- src/Components/Common/Navbar.js
+++ src/Components/Common/Navbar.js
@@ -2,13 +2,12 @@ import React from "react";
 import Logo from "../../Assets/logo.png";
 const Navbar = () => {
   return (
-    <div className="fixed top-0 right-0 left-0 z-50 w-full bg-gray-100 h-20">
-      <div className="mx-auto container flex ">
+    <div className="fixed top-0 right-0 left-0 z-50 w-full bg-gray-100 h-24">
+      <div className="mx-auto container flex mt-3">
         <div className="w-1/2">
           <img className=" pt-5 h-auto" alt="" src={Logo} />
         </div>
-
-        <div className="pt-3 w-full flex justify-end ">
+        <div className="pt-4 w-full flex justify-end ">
           <button className="text-xl border py-1 px-4 rounded-xl text-blue-700 font-bold border-blue-700">
             Masuk
           </button>
--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,12 +1,24 @@
 import React from "react";
 import Navbar from "../Components/Common/Navbar";
 import Footer from "../Components/Common/Footer";
+import Hero1 from '../Assets/hero1.png'
+import { useState } from "react";
 
 function Homepage() {
+  const [query, setQuery] = useState("");
+
   return (
     <div>
       <Navbar />
-      <div></div>
+      <div className="mx-auto container flex justify-center mt-36 h-full w-full">
+        <img src={Hero1} alt="" className="z-10 absolute"/>
+        <div class="z-40 relative container mx-auto flex flex-col items-center justify-center mt-52 p-12 text-gray-900 antialiased">
+          <div class="mb-8 px-6 py-3 border border-gray-200 bg-white max-w-lg max-h-min border-opacity-75 rounded-lg shadow-lg w-full space-x-6 flex items-center">
+            <input type="search" class="w-full bg-transparent text-base focus:outline-none" placeholder="Apa yang anda cari?"/>
+            <button class="focus:outline-none"><i class="fa fa-search text-gray-500"></i></button>
+          </div>
+        </div>
+      </div>
       <div></div>
       <div></div>
       {/* <Footer /> */}
