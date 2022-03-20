c52830cef005ea7ca8b624862395993c247c6a5f
zen <ulel2123@gmail.com>
2022-03-21 03:47:04 +0700

    login regis sesuai ui

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -86,125 +86,132 @@ const Login = () => {
 
   return (
     <div>
-      <div className="h-screen grid grid-cols-1 lg:grid-cols-2 ">
+      <div className="h-screen">
         <div className="left ">
-          <button>
-            <Link to="/">
-              <ChevronLeftIcon className="h-8 w-8 mt-10 ml-4  md:ml-10 text-corn black-500" />
-            </Link>
-          </button>
-          <div className="mx-8 md:mx-auto mt-6 mb-16">
-            <div className="max-w-md  font-nunito m-auto">
-              <div className="flex items-center flex-col">
-                <img src={logo} alt="kuRentalkan" className="w-45 " />
-              </div>
-              <h1 className="font-bold text-2xl mb-16 mt-16">Masuk</h1>
-              <div className="flex flex-col">
-                <div>
-                  <form className="flex flex-col">
-                    <label className="text-base" htmlFor="Email Address">
-                      Email Address
-                    </label>
-                    <input
-                      onChange={(e) => setEmail(e.target.value)}
-                      className="border-b-2 max-w-md h-10 mb-1 focus:outline-none"
-                      id="email"
-                      type="text"
-                      name="email"
-                    />
-                    {checkedEmail !== "" && (
-                      <p className="text-red-500 my-2 font-nunito">
-                        {checkedEmail}
-                      </p>
-                    )}
-                  </form>
-                </div>
-                <div>
-                  <div className="relative">
-                    <form className=" flex flex-col">
-                      <label className="text-base" htmlFor="Password">
-                        Password
-                      </label>
-                      <div className="flex items-center justify-evenly">
-                        <input
-                          onChange={(e) => setPassword(e.target.value)}
-                          className="mb-1 border-b-2 w-full h-10 focus:outline-none"
-                          type={!toggle ? "password" : "text"}
-                          name="password"
-                          id="password"
-                        />
+          <div className="flex justify-between">
+            <button>
+              <Link to="/">
+                <ChevronLeftIcon className="h-8 w-8 mt-10 ml-4  md:ml-10 text-corn black-500" />
+              </Link>
+            </button>
+            <div className="flex mt-10 items-center flex-col">
+              <img src={logo} alt="kuRentalkan" className="w-45 " />
+            </div>
+            <div>
 
-                        <button
-                          onClick={(e) => {
-                            e.preventDefault();
-                            setToggle(!toggle);
-                          }}
-                        >
-                          {!toggle ? (
-                            <EyeIcon className="absolute h-5 right-0 bottom-5" />
-                          ) : (
-                            <EyeOffIcon className="absolute h-5 right-0 bottom-5" />
-                          )}
-                        </button>
-                      </div>
+            </div>
+          </div>
+          <div style={{backgroundImage: "url(/img/vectorLogin.png)", backgroundRepeat: "no-repeat", backgroundPosition: "center"}}>
+            <div className="mx-8 md:mx-auto mt-20 mb-16 border-2 rounded-2xl bg-white shadow-lg w-1/3 pb-8">
+              <div className="max-w-md  font-nunito m-auto">
+                <h1 className="font-bold text-2xl mb-16 mt-16">Masuk</h1>
+                <div className="flex flex-col">
+                  <div>
+                    <form className="flex flex-col">
+                      <label className="text-base" htmlFor="Email Address">
+                        Email Address
+                      </label>
+                      <input
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="border-b-2 max-w-md h-10 mb-1 focus:outline-none"
+                        id="email"
+                        type="text"
+                        name="email"
+                      />
+                      {checkedEmail !== "" && (
+                        <p className="text-red-500 my-2 font-nunito">
+                          {checkedEmail}
+                        </p>
+                      )}
                     </form>
                   </div>
-                  {checkedPassword !== "" && (
-                    <p className=" text-red-500 my-2 font-nunito">
-                      {checkedPassword}
-                    </p>
-                  )}
-                  <span className="my-2 text-blue-600 font-nunito">
-                    <Link to="/lupapass">Lupa kata sandi? </Link>
-                  </span>
-                  <div className="flex items-center flex-col">
-                    {checkedLogin !== "" && (
+                  <div>
+                    <div className="relative">
+                      <form className=" flex flex-col">
+                        <label className="text-base" htmlFor="Password">
+                          Password
+                        </label>
+                        <div className="flex items-center justify-evenly">
+                          <input
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="mb-1 border-b-2 w-full h-10 focus:outline-none"
+                            type={!toggle ? "password" : "text"}
+                            name="password"
+                            id="password"
+                          />
+
+                          <button
+                            onClick={(e) => {
+                              e.preventDefault();
+                              setToggle(!toggle);
+                            }}
+                          >
+                            {!toggle ? (
+                              <EyeIcon className="absolute h-5 right-0 bottom-5" />
+                            ) : (
+                              <EyeOffIcon className="absolute h-5 right-0 bottom-5" />
+                            )}
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                    {checkedPassword !== "" && (
                       <p className=" text-red-500 my-2 font-nunito">
-                        {checkedLogin}
+                        {checkedPassword}
                       </p>
                     )}
-                    <button
-                      type="submit"
-                      className={`${
-                        email !== "" && password !== ""
-                          ? "bg-blue-600 text-white"
-                          : "bg-gray-300 text-gray-600"
-                      } w-full lg:w-auto mt-10 mb-4 lg:px-24 py-2 text-base font-bold radius rounded-full lg:rounded-md`}
-                      onClick={() => {
-                        HandleLogin();
-                      }}
-                    >
-                      Masuk
-                    </button>
-                    <span className="hidden lg:block my-2 text-blue-500 font-nunito">
-                      <Link to="/register">Belum punya akun? Daftar </Link>
+                    <span className="my-2 text-blue-600 font-nunito">
+                      <Link to="/lupapass">Lupa kata sandi? </Link>
                     </span>
+                    <div className="flex items-center flex-col">
+                      {checkedLogin !== "" && (
+                        <p className=" text-red-500 my-2 font-nunito">
+                          {checkedLogin}
+                        </p>
+                      )}
+                      <button
+                        type="submit"
+                        className={`${
+                          email !== "" && password !== ""
+                            ? "bg-blue-600 text-white"
+                            : "bg-gray-300 text-gray-600"
+                        } w-full lg:w-auto mt-10 mb-4 lg:px-24 py-2 text-base font-bold radius rounded-full lg:rounded-md`}
+                        onClick={() => {
+                          HandleLogin();
+                        }}
+                      >
+                        Masuk
+                      </button>
+                      <span className="hidden lg:block my-2 text-blue-500 font-nunito">
+                        <Link to="/register">Belum punya akun? Daftar </Link>
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="items-center py-2">
-                <div className="flex my-2 text-gray-600 gap-x-2 items-center justify-center">
-                  <div className="h-px w-52 bg-gray-300" />
-                  atau
-                  <div className="h-px w-52 bg-gray-300" />
+                <div className="items-center py-2">
+                  <div className="flex my-2 text-gray-600 gap-x-2 items-center justify-center">
+                    <div className="h-px w-52 bg-gray-300" />
+                    atau
+                    <div className="h-px w-52 bg-gray-300" />
+                  </div>
+                </div>
+                <div className="flex justify-center lg:hidden space-x-4">
+                  <LoginBy thirdParty={"Google"} />
+                  <LoginBy thirdParty={"kedua"} />
+                </div>
+                <div className="space-y-4 hidden lg:block">
+                  <LoginBy thirdParty={"Google"} />
+                  <LoginBy thirdParty={"kedua"} />
                 </div>
               </div>
-              <div className="flex justify-center lg:hidden space-x-4">
-                <LoginBy thirdParty={"Google"} />
-                <LoginBy thirdParty={"kedua"} />
+              <div className="hidden lg:flex items-center flex-col">
+                <span className="mt-10 font-nunito text-center text-sm md:text-left">
+                  Butuh bantuan?{" "}
+                  <Link to="/faq" className="text-blue-500">
+                    Hubungi Kurentalkan Care
+                  </Link>
+                </span>
               </div>
-              <div className="space-y-4 hidden lg:block">
-                <LoginBy thirdParty={"Google"} />
-                <LoginBy thirdParty={"kedua"} />
-              </div>
-            </div>
-            <div className="hidden lg:flex items-center flex-col">
-              <span className="mt-10 font-nunito text-center text-sm md:text-left">
-                Butuh bantuan?{" "}
-                <Link to="/faq" className="text-blue-500">
-                  Hubungi Kurentalkan Care
-                </Link>
-              </span>
             </div>
           </div>
         </div>
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { EyeIcon, EyeOffIcon, ChevronLeftIcon } from "@heroicons/react/outline";
 import logo from "../Assets/logo.png";
+import vectorRegis from "../Assets/vectorRegis.png";
 import LoginBy from "../Components/LoginBy/LoginBy";
 import ModalComponent from "../Components/Modal/Modal";
 import { Link } from "react-router-dom";
@@ -114,163 +115,169 @@ function Register() {
   return (
     <div className="font-nunito">
       <div>
-        <div className="h-screen grid grid-cols-1 lg:grid-cols-2 ">
+        <div className="h-screen">
           <div className="left ">
-            <button>
-              <Link to="/">
-                <ChevronLeftIcon
-                  onClick={() => toggleOtp(false)}
-                  className="h-8 w-8 mt-10 ml-4  md:ml-10 text-corn black-500"
-                />
-              </Link>
-            </button>
-            <div className="mx-8 md:mx-auto mb-16">
-              <div className="max-w-md  font-nunito m-auto">
-                <div className="flex items-center flex-col">
-                  <img src={logo} alt="kuRentalkan" className="w-45 " />
-                </div>
-                <h1 className="font-bold flex flex-col items-center text-2xl mb-5 mt-5">
-                  Buat Akun
-                </h1>
-                <div className="flex flex-col">
+            <div className="flex justify-between">
+              <button>
+                <Link to="/">
+                  <ChevronLeftIcon
+                    onClick={() => toggleOtp(false)}
+                    className="h-8 w-8 mt-10 ml-4  md:ml-10 text-corn black-500"
+                  />
+                </Link>
+              </button>
+              <div className="flex items-center flex-col mt-10">
+                <img src={logo} alt="kuRentalkan" className="w-45 " />
+              </div>
+              <div></div>
+            </div>
+            <div className="grid grid-cols-2 w-2/3 mx-auto mt-20">
+              <img className="my-auto" src={vectorRegis} alt=""></img>
+              <div className="mx-8 md:mx-auto mb-16 bg-white border-2 rounded-xl shadow-lg p-12">
+                <div className="max-w-md  font-nunito m-auto">
+                  <h1 className="font-bold flex flex-col items-center text-2xl mb-5 mt-5">
+                    Buat Akun
+                  </h1>
                   <div className="flex flex-col">
-                    <label className="text-base" htmlFor="Email Address">
-                      Email Address
-                    </label>
-                    <input
-                      onChange={(e) => setEmail(e.target.value)}
-                      className="border-b-2 w-full py-1 focus:outline-none"
-                      id="email"
-                      type="text"
-                      name="email"
-                    />
-                    {checkedEmail.length > 0 && (
-                      <p className="text-red-500">{checkedEmail}</p>
-                    )}
-                  </div>
-                  <div>
-                    <div className="relative flex flex-col py-2">
-                      <label className="text-base" htmlFor="Password">
-                        Password
-                      </label>
-                      <div className="flex items-center justify-evenly">
-                        <input
-                          onChange={(e) => setPassword(e.target.value)}
-                          className="border-b-2 w-full py-1 focus:outline-none"
-                          type={toggle ? "password" : "text"}
-                          name="password"
-                          id="password"
-                        />
-                        <button
-                          onClick={(e) => {
-                            e.preventDefault();
-                            setToggle(!toggle);
-                          }}
-                        >
-                          {toggle ? (
-                            <EyeIcon className="absolute h-5 right-0 bottom-5" />
-                          ) : (
-                            <EyeOffIcon className="absolute h-5 right-0 bottom-5" />
-                          )}
-                        </button>
-                      </div>
-                    </div>
-                    {checkedPassword.length > 0 && (
-                      <p className=" text-red-500">{checkedPassword}</p>
-                    )}
-                  </div>
-                  <div>
-                    <div className="flex flex-col py-2">
-                      <label className="text-base" htmlFor="Nomor Telepon">
-                        Nomor Telepon
+                    <div className="flex flex-col">
+                      <label className="text-base" htmlFor="Email Address">
+                        Email Address
                       </label>
                       <input
-                        onChange={(e) => setPhonenumber(e.target.value)}
+                        onChange={(e) => setEmail(e.target.value)}
                         className="border-b-2 w-full py-1 focus:outline-none"
-                        id="nomor"
-                        type="number"
-                        name="nomor"
+                        id="email"
+                        type="text"
+                        name="email"
                       />
+                      {checkedEmail.length > 0 && (
+                        <p className="text-red-500">{checkedEmail}</p>
+                      )}
                     </div>
-                    {checkedPhone.length > 0 && (
-                      <p className=" text-red-500">{checkedPhone}</p>
-                    )}
-                  </div>
-                  {/* <div className="hidden lg:block relative">
-                    <div className=" flex flex-col py-2">
-                      <label className="text-base" htmlFor="OTP">
-                        Konfirmasi OTP
-                      </label>
-                      <div className="flex items-center">
-                        <input
-                          onChange={(e) => setOTP(e.target.value)}
-                          className="border-b-2 w-full py-1 mr-2 focus:outline-none"
-                          type="number"
-                          name="OTP"
-                        />
-                        <button
-                          onClick={resendOTP}
-                          className={
-                            disableButton
-                              ? "text-white  bg-gray-300 rounded-md h-11 w-40  "
-                              : "text-white bg-gray-300 hover:bg-blue-500 hover:text-white rounded-md h-11 w-40"
-                          }
-                          disabled={disableButton}
-                        >
-                          {disableButton
-                            ? `Kirim OTP (${seconds})`
-                            : "Kirim OTP"}
-                        </button>
+                    <div>
+                      <div className="relative flex flex-col py-2">
+                        <label className="text-base" htmlFor="Password">
+                          Password
+                        </label>
+                        <div className="flex items-center justify-evenly">
+                          <input
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="border-b-2 w-full py-1 focus:outline-none"
+                            type={toggle ? "password" : "text"}
+                            name="password"
+                            id="password"
+                          />
+                          <button
+                            onClick={(e) => {
+                              e.preventDefault();
+                              setToggle(!toggle);
+                            }}
+                          >
+                            {toggle ? (
+                              <EyeIcon className="absolute h-5 right-0 bottom-5" />
+                            ) : (
+                              <EyeOffIcon className="absolute h-5 right-0 bottom-5" />
+                            )}
+                          </button>
+                        </div>
                       </div>
+                      {checkedPassword.length > 0 && (
+                        <p className=" text-red-500">{checkedPassword}</p>
+                      )}
                     </div>
-                    {checkedOTP.length > 0 && (
-                      <p className=" text-red-500">{checkedOTP}</p>
-                    )}
-                  </div> */}
-                </div>
-                <div className="flex items-center flex-col">
-                  <button
-                    type="submit"
-                    className={`${
-                      email && email && phonenumber !== ""
-                        ? "bg-blue-500 text-white"
-                        : "bg-gray-300"
-                    } mt-6 mb-4 text-base font-bold w-full lg:w-60 h-10 radius rounded-full lg:rounded-md`}
-                    onClick={() => {
-                      handleRegister();
-                    }}
-                  >
-                    Daftar Sekarang
-                  </button>
-                  <p className="mb-4 hidden lg:block">
-                    Sudah punya akun?{" "}
-                    <a className="text-blue-300">
-                      <Link to="/login">Masuk </Link>
-                    </a>
-                  </p>
-                </div>
-                <div className="items-center ">
-                  <div className="flex my-2 text-gray-600 gap-x-2 items-center justify-center">
-                    <div className="h-px w-52 bg-gray-300" />
-                    atau
-                    <div className="h-px w-52 bg-gray-300" />
+                    <div>
+                      <div className="flex flex-col py-2">
+                        <label className="text-base" htmlFor="Nomor Telepon">
+                          Nomor Telepon
+                        </label>
+                        <input
+                          onChange={(e) => setPhonenumber(e.target.value)}
+                          className="border-b-2 w-full py-1 focus:outline-none"
+                          id="nomor"
+                          type="number"
+                          name="nomor"
+                        />
+                      </div>
+                      {checkedPhone.length > 0 && (
+                        <p className=" text-red-500">{checkedPhone}</p>
+                      )}
+                    </div>
+                    {/* <div className="hidden lg:block relative">
+                      <div className=" flex flex-col py-2">
+                        <label className="text-base" htmlFor="OTP">
+                          Konfirmasi OTP
+                        </label>
+                        <div className="flex items-center">
+                          <input
+                            onChange={(e) => setOTP(e.target.value)}
+                            className="border-b-2 w-full py-1 mr-2 focus:outline-none"
+                            type="number"
+                            name="OTP"
+                          />
+                          <button
+                            onClick={resendOTP}
+                            className={
+                              disableButton
+                                ? "text-white  bg-gray-300 rounded-md h-11 w-40  "
+                                : "text-white bg-gray-300 hover:bg-blue-500 hover:text-white rounded-md h-11 w-40"
+                            }
+                            disabled={disableButton}
+                          >
+                            {disableButton
+                              ? `Kirim OTP (${seconds})`
+                              : "Kirim OTP"}
+                          </button>
+                        </div>
+                      </div>
+                      {checkedOTP.length > 0 && (
+                        <p className=" text-red-500">{checkedOTP}</p>
+                      )}
+                    </div> */}
                   </div>
-                </div>
-                <div className="flex justify-center lg:hidden space-x-4">
-                  <LoginBy thirdParty={"Google"} />
-                  <LoginBy thirdParty={"MyValue"} />
-                </div>
-                <div className="space-y-4 hidden lg:block">
-                  <LoginBy thirdParty={"Google"} />
-                  <LoginBy thirdParty={"MyValue"} />
-                </div>
-                <div className="hidden lg:flex items-center text-center flex-col text-sm">
-                  <span className="mt-10">
-                    Dengan melanjutkan, Anda menyetujui{" "}
-                    <a className="text-blue-300">Syarat dan Ketentuan kami</a>{" "}
-                    dan juga{" "}
-                    <a className="text-blue-300">Kebijakan Privasi kami</a>
-                  </span>
+                  <div className="flex items-center flex-col">
+                    <button
+                      type="submit"
+                      className={`${
+                        email && email && phonenumber !== ""
+                          ? "bg-blue-500 text-white"
+                          : "bg-gray-300"
+                      } mt-6 mb-4 text-base font-bold w-full lg:w-60 h-10 radius rounded-full lg:rounded-md`}
+                      onClick={() => {
+                        handleRegister();
+                      }}
+                    >
+                      Daftar Sekarang
+                    </button>
+                    <p className="mb-4 hidden lg:block">
+                      Sudah punya akun?{" "}
+                      <a className="text-blue-300">
+                        <Link to="/login">Masuk </Link>
+                      </a>
+                    </p>
+                  </div>
+                  <div className="items-center ">
+                    <div className="flex my-2 text-gray-600 gap-x-2 items-center justify-center">
+                      <div className="h-px w-52 bg-gray-300" />
+                      atau
+                      <div className="h-px w-52 bg-gray-300" />
+                    </div>
+                  </div>
+                  <div className="flex justify-center lg:hidden space-x-4">
+                    <LoginBy thirdParty={"Google"} />
+                    <LoginBy thirdParty={"MyValue"} />
+                  </div>
+                  <div className="space-y-4 hidden lg:block">
+                    <LoginBy thirdParty={"Google"} />
+                    <LoginBy thirdParty={"MyValue"} />
+                  </div>
+                  <div className="hidden lg:flex items-center text-center flex-col text-sm">
+                    <span className="mt-10">
+                      Dengan melanjutkan, Anda menyetujui{" "}
+                      <a className="text-blue-300">Syarat dan Ketentuan kami</a>{" "}
+                      dan juga{" "}
+                      <a className="text-blue-300">Kebijakan Privasi kami</a>
+                    </span>
+                  </div>
                 </div>
               </div>
             </div>
