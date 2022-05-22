dcdd49c1c54a1c567981a9692c11c82cd25df208
zen <ulel2123@gmail.com>
2022-05-22 19:44:18 +0700

    checkout click 3x berubah

--- server/server.js
+++ server/server.js
@@ -1,43 +1,48 @@
-const express = require ('express')
+const express = require('express')
 const app = express()
-const randstring = require ('randomstring')
-
+const bodyParser = require('body-parser')
+  
+// create application/json parser
+const jsonParser = bodyParser.json()
+ 
+const randstring = require('randomstring')
 const midtransClient = require('midtrans-client');
+
 // Create Snap API instance
 let snap = new midtransClient.Snap({
-        // Set to true if you want Production Environment (accept real transaction).
-        isProduction : false,
-        serverKey : 'SB-Mid-server-3tRgnYTQ_dItAv3674gNnexW'
-    });
-
- 
-    app.get("/api", (req,res)=>{
-        snap.createTransaction({
-            "transaction_details": {
-                "order_id": randstring.generate(),
-                "gross_amount": 40000000
-            },
-            "credit_card":{
-                "secure" : true
-            },
-            "customer_details": {
-                "first_name": "Dias",
-                "last_name": "Gay",
-                "email": "diasgay@gmail.com",
-                "phone": "08111222333"
-            }})
-            .then((transaction)=>{
-                // transaction token
-                transactionToken = transaction.token;
-                console.log('transactionToken:',transactionToken);
-                
-           
-            })
-        res.send(transactionToken)
-        console.log("success")
-        console.log(req.body)
-    }) 
-    
+    // Set to true if you want Production Environment (accept real transaction).
+    isProduction: false,
+    serverKey: 'SB-Mid-server-3tRgnYTQ_dItAv3674gNnexW'
+});
 
 
-app.listen(5000, () => {console.log("server started on 5000") })
+app.post("/api", jsonParser, (req, res) => {
+    snap.createTransaction({
+        "transaction_details": {
+            "order_id": randstring.generate(),
+            "gross_amount": Number(req.body.amount)
+        },
+        "credit_card": {
+            "secure": true
+        },
+        "customer_details": {
+            "first_name": "Dias",
+            "last_name": "Gay",
+            "email": "diasgay@gmail.com",
+            "phone": "08111222333"
+        }
+    })
+        .then((transaction) => {
+            // transaction token
+            transactionToken = transaction.token;
+            console.log('transactionToken:', transactionToken);
+
+
+        })
+    res.send(transactionToken)
+    console.log(`success, amount :${req.body.amount}`)
+})
+
+
+
+app.listen(5000, () => { console.log("server started on 5000") })
